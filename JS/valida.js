697576086eb9ab1afaf59231d0358905fc3d530c
ItamarDuoc <it.carrasco@duocuc.cl>
2024-05-12 19:53:28 -0400

    Validaciones
    Validaciones provisionales agregadas, se mejorarán pronto para que no muestren todos los mensajes de error al mismo tiempo

--- JS/valida.js
+++ JS/valida.js
@@ -12,36 +12,66 @@ form.addEventListener("submit", e=>{
     e.preventDefault(); // Evita que el formulario se envie de forma automatica
     let errores = "";
     let regexNombreApellido = /^[A-Za-z\s]+$/;
+    let regexEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,4})+$/;
+    let regexNumber = /^\d+$/;
+    error.innerHTML = ""
+
     // Validaciones para el nombre
     nombre.value = nombre.value.trim();
     if (nombre.value == '' || nombre.value == undefined) {
-        alert("Please enter your name.");
         errores += 'El nombre no debe estar vacio <br>';
     }
     if (nombre.value.length < 3 || nombre.value.length > 20){
-        alert("El nombre debe tener de 3-20 caracteres");
         errores += 'El nombre debe tener de 3-20 Caracteres <br>';
     }
     if (!regexNombreApellido.test(nombre.value)){
-        alert("El nombre solo debe contener letras o espacios")
         errores += 'El nombre solo debe contener letras o espacios <br>'
     }
+
     // Validaciones para el apellido
     apellido.value = apellido.value.trim();
     if (apellido.value == '' || apellido.value == undefined) {
-        alert("Please enter your lastname.");
         errores += 'El apellido no debe estar vacio <br>';
     }
     if (apellido.value.length < 3 || apellido.value.length > 20){
-        alert("El apellido debe tener de 3-20 caracteres");
         errores += 'El apellido debe tener de 3-20 Caracteres <br>';
     }
     if (!regexNombreApellido.test(apellido.value)){
-        alert("El apellido solo debe contener letras o espacios")
         errores += 'El apellido solo debe contener letras o espacios <br>'
     }
+
     // Validaciones para el email
+    email.value = email.value.trim();
+    if (email.value == '' || email.value == undefined){
+        errores += 'El email no debe estar vacio <br>'
+    }
+    if (!regexEmail.test(email.value)){
+        errores += 'El email debe seguir el patron nombre@dominio.com <br>'
+    }
+
     // Validaciones para telefono
+    numero.value = numero.value.trim()
+    if (numero.value == '' || numero.value == undefined){
+        errores += 'El numero no debe estar vacio <br>'
+    }
+    if (numero.value.length != 9){
+        errores += 'El numero debe contener 9 caracteres <br>';
+    }
+    if (!regexNumber.test(numero.value)){
+        errores += 'El numero debe seguir el patron 955552222 <br>'
+    }
     // Validaciones para el mensaje
+    if (mensaje.value == '' || mensaje.value == undefined){
+        errores += 'El mensaje no debe estar vacio <br>'
+    }
+    if (mensaje.value.length > 1000){
+        errores += 'El mensaje no debe superar los 1000 caracteres <br>'
+    }
+    // Finalizacion
     error.innerHTML = errores
+    if (errores === "") {
+        form.submit();
+    } else {
+        error.innerHTML = errores;
+    }
 })
